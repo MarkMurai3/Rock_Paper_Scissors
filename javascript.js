23979dad60b60659bf1546443274059d6d6b7ddd
Futureland3 <markmurai3@gmail.com>
2022-10-23 17:27:36 +0200

    Rework functions and variables, add buttons

--- javascript.js
+++ javascript.js
@@ -1,85 +1,56 @@
-//if I chose Rock (0):
-    //if computer chose Rock(0): Tie
-    //if computer chose Paper (1): I lose
-    //if computer chose Scissor(2): I win
-//if I chose Paper(1):
-    //if computer chose Rock(0): I win
-    //if computer chose Paper(1): Tie
-    //if computer chose Scissor(2): I lose
-//if I chose Scissor(2):
-    //if computer chose Rock(0): I lose
-    //if computer chose Paper(1): I win
-    //if computer chose Scissor(2): Tie
+let playerScore = 0
+let computerScore = 0
+const buttons = document.querySelectorAll('input')
 
-//The user has to input an answer.
-//Then the computer generates randomly an answer too. 
-//The computer compares the 2 answers and gives the user a text based on that
-//Write a NEW function called game(). Call the playRound function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
-
-
-const getComputerChoice = () => {
-  const randomNumber = Math.floor(Math.random()*3);
-  switch (randomNumber) {
-    case 0:
-      return "Rock";
-      break;
-    case 1:
-      return "Paper";
-      break;
-    case 2:
-      return "Scissor";
-      break;
-  }
-}
-const input = prompt("Rock, Paper or Scissor?");
-
-const playerSelection = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
-
-console.log(playerSelection);
-
-const computerSelection = getComputerChoice();
-
-console.log(computerSelection);
-
-let round = 5;
-
-function roundUp() {
-  round += 1;
+function computerPlay() {
+    let choices = ['Rock', 'Paper', 'Scissors']
+    return choices[Math.floor(Math.random() * choices.length)]
 }
 
-function playRound(playerSelection, computerSelection) {
-  let round = 1
-if (playerSelection === computerSelection){
-  return ("It's a Tie!");
-}
-else if (playerSelection === "Rock") {
-  if (computerSelection === "Scissor") {
-    return ("You Win! Rock beats Scissor");
-  }
-  else 
-    return ("You Lose! Paper beats Rock");
-  }
-else if (playerSelection === "Paper") {
-  if (computerSelection === "Rock") {
-    return ("You Win! Paper beats Rock");
-  }
-  else 
-    return ("You Lose! Scissor beats Paper");
-}
-else if (playerSelection === "Scissor") {
-  if (computerSelection === "Paper") {
-    return ("You Win! Scissor beats Paper");
-  }
-  else 
-    return ("You Lose! Rock beats Scissor");
-}
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
 }
 
-console.log(playRound(playerSelection, computerSelection));
+function playRound(playerSelection) {
+    let computerSelection = computerPlay()
+    let result = ""
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    playRound 
-    //This part is not done yet, just ignore it for now guys
-  }
+    if ((playerSelection == 'Rock' && computerSelection == 'Scissors') ||
+        (playerSelection == 'Scissors' && computerSelection == 'Paper') ||
+        (playerSelection == 'Paper' && computerSelection == 'Rock')) {
+        
+        playerScore += 1
+        result = ('You win! ' + playerSelection + ' beats ' + computerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+
+        if (playerScore == 5) {
+            result += '<br><br>You won the game! Reload the page to play again'
+            disableButtons()
+        }
+    }
+    else if (playerSelection == computerSelection) {
+        result = ('It\'s a tie. You both chose ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+    }
+    else {
+        computerScore += 1
+        result = ('You lose! ' + computerSelection + ' beats ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+
+        if (computerScore == 5) {
+            result += '<br><br>I won the game! Reload the page to play again'
+            disableButtons()
+        }
+    }
+
+    document.getElementById('result').innerHTML = result
+    return
 }
+
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        playRound(button.value)
+    })
+})
