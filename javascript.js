688df12f095ffe1455e13feb0046c6d2c87f3c37
Futureland3 <markmurai3@gmail.com>
2022-11-02 00:04:38 +0100

    Remove console.log feature, Add functional buttons, Disable buttons at 5th point

--- javascript.js
+++ javascript.js
@@ -1,64 +1,49 @@
-
-//this one generates the computer's answer
-const getComputerChoice = () => ["Rock","Paper","Scissor"][Math.floor(Math.random()*3)];
-
-//this one I found online and I am too afraid to delete because it may come in handy
-//function getPlayerSelection(){
-//  return window.prompt("Enter your choice:").toLowerCase()
-//}
-
 let playerScore = 0
 let computerScore = 0
+const buttons = document.querySelectorAll('input');
 
+const getComputerChoice = () => ["Rock","Paper","Scissors"][Math.floor(Math.random()*3)];
 
-//this one generates the Winner and Loser 
-function playRound() {
-//this one inputs the player's answer
-let input = prompt("Rock, Paper or Scissor?");
-let playerSelection = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
-console.log(playerSelection);
-//this one shows the Computer's answer
+function disableButtons() {
+  buttons.forEach(elem =>{
+    elem.disabled = true
+  })
+}
+
+function playRound(playerSelection) {
 let computerSelection = getComputerChoice();
-console.log(computerSelection);
+let result = "";
 if (playerSelection === computerSelection){
-  return ("It's a Tie!" + " Player: " + (playerScore) + " Computer: " + computerScore) ;
+  result = ("It's a Tie!" + "You both chose " + playerSelection + ". Player: " + (playerScore) + " Computer: " + computerScore);
 }
-else if ((playerSelection == "Rock" && computerSelection == "Scissor") ||
+else if ((playerSelection == "Rock" && computerSelection == "Scissors") ||
         (playerSelection == "Paper" && computerSelection == "Rock") ||
-        (playerSelection == "Scissor" && computerSelection == "Paper"))
+        (playerSelection == "Scissors" && computerSelection == "Paper")) {
+        result = ("You Win! " + playerSelection + " beats " + computerSelection + ". Player: " + (playerScore+=1) + " Computer: " + computerScore)
 
-        return ("You Win! " + playerSelection + " beats " + computerSelection + ". Player: " + (playerScore += 1) + " Computer: " + computerScore) 
+        if (playerScore == 5) {
+          result += "<br>You have won the game! Congratulations!"
+          disableButtons();
+        }
+        }
         
         
-else 
-  return ("You Lose! " + computerSelection + " beats " + playerSelection + ". Player: " + playerScore + " Computer: " + (computerScore +=1) )
+        
+else {
+  result = ("You Lose! " + computerSelection + " beats " + playerSelection + ". Player: " + playerScore + " Computer: " + (computerScore +=1))
+  if (computerScore == 5) {
+    result += "<br>You have lost the game! Sucker!"
+    disableButtons();
+  }
+}
+document.getElementById('result').innerHTML = result
+return
 }
 
-//this loop shows the You Win or You Lose in the console after someone got 5 points
-function game() {
-  while (computerScore !== 5 || playerScore !==5) {
-  console.log(playRound())
-    if (computerScore ===5) {
-     return console.log("You lost the game! To replay the game refresh the page!")
-     }
-   else if (playerScore ===5){
-     return console.log("You won the game! To replay the game refresh the page!")
-   }
-   }
-}
 
-game();
 
-//This is the previous code I used for the game() function. This one allows only 5 rounds and the winner is whoever had the most points.
-//function game() {
-//  for (let i = 0; i < 5; i++) {
-//  console.log(playRound())}
-//  if (computerScore === playerScore) {
-//    return console.log("You have the same score! It's a tie! To replay the game refresh the page!")
-//     }
-//   else if (computerScore > playerScore) {
-//     return console.log("You lost the game! To replay the game refresh the page!")
-//     }
-//   else 
-//     return console.log("You won the game! To replay the game refresh the page!")   
-//}
+buttons.forEach(button =>{
+  button.addEventListener('click', function(){
+      playRound(button.value)
+  })
+})
